861ff9dd36c48718600ae2592629dbd182c782a5
Nishat Hoque <hoquen0397@gmail.com>
2021-02-16 15:52:08 -0500

    update

--- src/notes/notes.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-'use strict';
-require('dotenv').config();
-const knex = require('knex');
-
-const knexInstance = knex({
-    client: 'pg',
-    connection: process.env.TEST_DB_URL
-});
